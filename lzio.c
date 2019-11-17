3ad4a640192a0f4af237e010d3f447260f71834f
zhangyuxi <615598813.com>
2019-11-17 21:20:28 +0800

    add lzio

--- lzio.c
+++ lzio.c
@@ -1,6 +1,6 @@
 /*
 ** $Id: lzio.c,v 1.37 2015/09/08 15:41:05 roberto Exp $
-** Buffered streams
+** Buffered streams 缓冲流
 ** See Copyright Notice in lua.h
 */
 
@@ -19,24 +19,25 @@
 #include "lstate.h"
 #include "lzio.h"
 
-// 所谓fill可能是指z->data->buff被fill了
+// 用于填充ZIO对象的缓存区 当z->n为0 如果要从ZIO中继续读取需要调用该函数
+// 调用reader函数 ZIO修改对象中的n和size
 int luaZ_fill (ZIO *z) {
   size_t size;
   lua_State *L = z->L;
   const char *buff;
   lua_unlock(L);
-  buff = z->reader(L, z->data, &size);
+  buff = z->reader(L, z->data, &size); // getF 从z->data中读取内容 size为读取到的长度 buff为读取到的内容
   lua_lock(L);
-  if (buff == NULL || size == 0)
+  if (buff == NULL || size == 0) // 读取结束
     return EOZ;
-  // 因为返回了一个值, 所以未使用的要去掉一个
-  z->n = size - 1;  /* discount char being returned */
+  z->n = size - 1;  /* discount char being returned 使用reader读取了size个 最后返回了一个 所以还剩size-1 */
   z->p = buff;
   // 上面让p指向了buff 返回了一个值那么p要后移一个
-  return cast_uchar(*(z->p++));
+  return cast_uchar(*(z->p++));  // *p++ 与 *(p++)一样 得到*p的值再对p自增
 }
 
 
+// 初始化一个ZIO对象
 void luaZ_init (lua_State *L, ZIO *z, lua_Reader reader, void *data) {
   z->L = L;
   z->reader = reader;
@@ -48,17 +49,20 @@ void luaZ_init (lua_State *L, ZIO *z, lua_Reader reader, void *data) {
 
 /* --------------------------------------------------------------- read --- */
 // 从z中读取n个字符到b中
+// 返回值是 n-读到的字节数
 size_t luaZ_read (ZIO *z, void *b, size_t n) {
   while (n) {
     size_t m;
-    if (z->n == 0) {  /* no bytes in buffer? */
-      if (luaZ_fill(z) == EOZ)  /* try to read more */
-        return n;  /* no more input; return number of missing bytes */
+    if (z->n == 0) {  /* no bytes in buffer? 剩余可以直接读取的部分为0 */
+      if (luaZ_fill(z) == EOZ)  /* try to read more 尝试填充缓存区 */
+        return n;  /* no more input; return number of missing bytes 已经读取到底了直接返回n */
       else {
+        // 填充成功了 luaZ_fill默认会返回一个字符 这里将默认消耗的字符放回去
         z->n++;  /* luaZ_fill consumed first byte; put it back */
         z->p--;
       }
     }
+    // m就是这一轮读取的字节数 取n和z->n的最小值
     m = (n <= z->n) ? n : z->n;  /* min. between n and z->n */
     memcpy(b, z->p, m);
     z->n -= m;
--- lzio.h
+++ lzio.h
@@ -1,6 +1,6 @@
 /*
 ** $Id: lzio.h,v 1.31 2015/09/08 15:41:05 roberto Exp $
-** Buffered streams
+** Buffered streams 缓冲流
 ** See Copyright Notice in lua.h
 */
 
@@ -17,15 +17,19 @@
 
 typedef struct Zio ZIO;
 
+// 从ZIO中读取一个字符
+// 如果n>0说明缓存区还有值 直接返回 让p后移
+// p=0 说明缓存区读完了 使用luaZ_fill填充缓存区
 #define zgetc(z)  (((z)->n--)>0 ?  cast_uchar(*(z)->p++) : luaZ_fill(z))
 
 
 typedef struct Mbuffer {
-  char *buffer;
-  size_t n;
-  size_t buffsize;
+  char *buffer;    // 缓存的实际区域
+  size_t n;        // 缓存元素的个数
+  size_t buffsize; // 缓存区的长度
 } Mbuffer;
 
+// 初始化缓存
 #define luaZ_initbuffer(L, buff) ((buff)->buffer = NULL, (buff)->buffsize = 0)
 
 #define luaZ_buffer(buff)	((buff)->buffer)
@@ -36,11 +40,13 @@ typedef struct Mbuffer {
 #define luaZ_resetbuffer(buff) ((buff)->n = 0)
 
 
+// 重新分配buff的空间
 #define luaZ_resizebuffer(L, buff, size) \
 	((buff)->buffer = luaM_reallocvchar(L, (buff)->buffer, \
 				(buff)->buffsize, size), \
 	(buff)->buffsize = size)
 
+// 释放buff占用的空间
 #define luaZ_freebuffer(L, buff)	luaZ_resizebuffer(L, buff, 0)
 
 
@@ -52,12 +58,17 @@ LUAI_FUNC size_t luaZ_read (ZIO* z, void *b, size_t n);	/* read next n bytes */
 
 /* --------- Private Part ------------------ */
 
+// n:p指向的区域可以读的字节数
+// p:可以读的缓存区 n指示了p可以读的长度 n=0时p没有意义
+// reader:通过reader读取值并修改为对应的n和p
+// data:额外的数据
+
 struct Zio {
-  size_t n;			/* bytes still unread */
-  const char *p;		/* current position in buffer */
-  lua_Reader reader;		/* reader function */
-  void *data;			/* additional data */
-  lua_State *L;			/* Lua state (for reader) */
+  size_t n;			/* bytes still unread 未读的字节数*/
+  const char *p;		/* current position in buffer 当前在buffer中的位置 */
+  lua_Reader reader;		/* reader function 读取函数 getF */
+  void *data;			/* additional data 额外的数据 */
+  lua_State *L;			/* Lua state (for reader) 当前reader所在的线程 */
 };
 
 

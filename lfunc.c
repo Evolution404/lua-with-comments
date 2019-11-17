6786956e61361c29a2cb59838d3bae6308ec646a
zhangyuxi <615598813.com>
2019-11-17 15:15:47 +0800

    add lfunc

--- lfunc.c
+++ lfunc.c
@@ -22,6 +22,7 @@
 
 
 
+// 传入CClosure的upvalue个数 分配内存,创建CClosure对象,设置nupvalues
 CClosure *luaF_newCclosure (lua_State *L, int n) {
   GCObject *o = luaC_newobj(L, LUA_TCCL, sizeCclosure(n));
   CClosure *c = gco2ccl(o);
@@ -30,6 +31,9 @@ CClosure *luaF_newCclosure (lua_State *L, int n) {
 }
 
 
+// 传入LClosure的upvalue个数 分配内存,创建LClosure对象,设置nupvalues
+// 比上一个函数多了初始化的工作
+// 可能的原因是上一个函数不需要初始化 少了这一步提高效率
 LClosure *luaF_newLclosure (lua_State *L, int n) {
   GCObject *o = luaC_newobj(L, LUA_TLCL, sizeLclosure(n));
   LClosure *c = gco2lcl(o);
@@ -42,36 +46,41 @@ LClosure *luaF_newLclosure (lua_State *L, int n) {
 /*
 ** fill a closure with new closed upvalues
 */
+// 为LClosure的upvalue设置closed状态的初始值
 void luaF_initupvals (lua_State *L, LClosure *cl) {
   int i;
   for (i = 0; i < cl->nupvalues; i++) {
     UpVal *uv = luaM_new(L, UpVal);
     uv->refcount = 1;
-    uv->v = &uv->u.value;  /* make it closed */
-    setnilvalue(uv->v);
+    uv->v = &uv->u.value;  /* make it closed 当uv->v指向内部共用体的value时说明是closed状态 */
+    setnilvalue(uv->v);    // 设置nil值只需要修改一下类型标记即可
     cl->upvals[i] = uv;
   }
 }
 
 
+// 在L_>openupval中查询upval
+// 如果查询不到在l_>openupval链表头新建upval为要查询的值
 UpVal *luaF_findupval (lua_State *L, StkId level) {
-  UpVal **pp = &L->openupval;
+  UpVal **pp = &L->openupval; // 后面有可能修改L->openupval所以这里使用了二级指针
   UpVal *p;
   UpVal *uv;
   lua_assert(isintwups(L) || L->openupval == NULL);
+  // 查找L->openupval链表 如果查询到直接返回
   while (*pp != NULL && (p = *pp)->v >= level) {
     lua_assert(upisopen(p));
-    if (p->v == level)  /* found a corresponding upvalue? */
-      return p;  /* return it */
+    if (p->v == level)  /* found a corresponding upvalue? 判断是否是查询的upval */
+      return p;  /* return it 查询到结果直接返回 */
     pp = &p->u.open.next;
   }
-  /* not found: create a new upvalue */
+  /* not found: create a new upvalue 查询不到创建新的upval */
   uv = luaM_new(L, UpVal);
   uv->refcount = 0;
-  uv->u.open.next = *pp;  /* link it to list of open upvalues */
+  uv->u.open.next = *pp;  /* link it to list of open upvalues 将新创建的upval放到L->openupval链表最开始 */
   uv->u.open.touched = 1;
   *pp = uv;
-  uv->v = level;  /* current value lives in the stack */
+  uv->v = level;  /* current value lives in the stack 新创建的upval存放在数据栈中 还没有拷贝到UpVal中(open状态) */
+  // 如果当前线程不在twups链表中 将当前线程加入链表中
   if (!isintwups(L)) {  /* thread not in list of threads with upvalues? */
     L->twups = G(L)->twups;  /* link it to the list */
     G(L)->twups = L;
@@ -80,6 +89,8 @@ UpVal *luaF_findupval (lua_State *L, StkId level) {
 }
 
 
+// 将level之上的所有upval进入closed状态(关闭)
+// 如果一个upval没有引用 释放该upval
 void luaF_close (lua_State *L, StkId level) {
   UpVal *uv;
   while (L->openupval != NULL && (uv = L->openupval)->v >= level) {
@@ -87,15 +98,16 @@ void luaF_close (lua_State *L, StkId level) {
     L->openupval = uv->u.open.next;  /* remove from 'open' list */
     if (uv->refcount == 0)  /* no references? */
       luaM_free(L, uv);  /* free upvalue */
-    else {
-      setobj(L, &uv->u.value, uv->v);  /* move value to upvalue slot */
-      uv->v = &uv->u.value;  /* now current value lives here */
+    else { // 设置upval进入closed状态
+      setobj(L, &uv->u.value, uv->v);  /* move value to upvalue slot 从栈上拷贝值 */
+      uv->v = &uv->u.value;  /* now current value lives here 修改指向值的指针 */
       luaC_upvalbarrier(L, uv);
     }
   }
 }
 
 
+// 创建proto对象 并初始化
 Proto *luaF_newproto (lua_State *L) {
   GCObject *o = luaC_newobj(L, LUA_TPROTO, sizeof(Proto));
   Proto *f = gco2p(o);
@@ -122,6 +134,7 @@ Proto *luaF_newproto (lua_State *L) {
 }
 
 
+// 释放一个proto对象
 void luaF_freeproto (lua_State *L, Proto *f) {
   luaM_freearray(L, f->code, f->sizecode);
   luaM_freearray(L, f->p, f->sizep);
@@ -137,6 +150,8 @@ void luaF_freeproto (lua_State *L, Proto *f) {
 ** Look for n-th local variable at line 'line' in function 'func'.
 ** Returns NULL if not found.
 */
+// 查询函数f中在pc行 第local_number个局部变量的名称
+// 查询不到返回NULL
 const char *luaF_getlocalname (const Proto *f, int local_number, int pc) {
   int i;
   for (i = 0; i<f->sizelocvars && f->locvars[i].startpc <= pc; i++) {
--- lfunc.h
+++ lfunc.h
@@ -11,14 +11,21 @@
 #include "lobject.h"
 
 
+// 计算Cclosure的大小
+// CClosure定义最后一项 TValue upvalue[1] 所有的upvalue都放在最后面
+// 所以计算大小就是CClosure+(n-1)个TValue的大小(CClosure中已经有了一个)
 #define sizeCclosure(n)	(cast(int, sizeof(CClosure)) + \
                          cast(int, sizeof(TValue)*((n)-1)))
 
+// 计算Lclosure的大小
+// 与上面不同的是LClosure最后面存的是指针不是实际的Upvalue
+// 所以是TValue的指针大小*(n-1)
 #define sizeLclosure(n)	(cast(int, sizeof(LClosure)) + \
                          cast(int, sizeof(TValue *)*((n)-1)))
 
 
 /* test whether thread is in 'twups' list */
+// L与twups不直接相等说明L在twups后面的链上
 #define isintwups(L)	(L->twups != L)
 
 
@@ -26,15 +33,20 @@
 ** maximum number of upvalues in a closure (both C and Lua). (Value
 ** must fit in a VM register.)
 */
+// 一个闭包的upvalue上限
 #define MAXUPVAL	255
 
 
 /*
 ** Upvalues for Lua closures
 */
+// Lua函数闭包变量
+// 闭包变量有open和closed状态
+// open状态是外层函数还没有返回 open字段用作当前作用域的闭包变量链表
+// closed状态是外层函数返回将闭包变量的值拷贝出来 放在value字段
 struct UpVal {
-  TValue *v;  /* points to stack or to its own value */
-  lu_mem refcount;  /* reference counter */
+  TValue *v;  /* points to stack or to its own value 指向变量的实际值 */
+  lu_mem refcount;  /* reference counter 引用计数 */
   union {
     struct {  /* (when open) */
       UpVal *next;  /* linked list */
@@ -44,6 +56,7 @@ struct UpVal {
   } u;
 };
 
+// 当v字段和value字段相等说明进入了closed状态
 #define upisopen(up)	((up)->v != &(up)->u.value)
 
 

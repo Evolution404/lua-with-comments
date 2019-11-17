fe1a8383a4bee746b0dbfc2226efba162b1f3018
zhangyuxi <615598813.com>
2019-11-17 18:16:57 +0800

    add lmem

--- lmem.c
+++ lmem.c
@@ -1,6 +1,6 @@
 /*
 ** $Id: lmem.c,v 1.91 2015/03/06 19:45:54 roberto Exp $
-** Interface to Memory Manager
+** Interface to Memory Manager 内存管理接口
 ** See Copyright Notice in lua.h
 */
 
@@ -40,27 +40,39 @@
 ** (any reallocation to an equal or smaller size cannot fail!)
 */
 
+// 有关 realloc 函数
+// void * frealloc (void *ud, void *ptr, size_t osize, size_t nsize);
+// 'osize' 是 old size， 'nsize' 是 new size
+// frealloc(ud, NULL, x, s) 分配一块s大小的内存(不管x)
+// frealloc(ud, p, x, 0) 释放p指向的内存块 这种情况下返回值是NULL
+//  特别的 frealloc(ud, NULL, 0, 0) 什么也不做 等价于 free(NULL)
+// 如果创建或重新分配空间失败应该返回NULL, 重新分配的空间小于等于当前空间时永远不会失败
+
 
 
 #define MINSIZEARRAY	4
 
 
+// 用来管理可变长数组 主要策略是:当数组空间不够时,扩大为原来空间的两倍
+// 内存管理部分使用了基于luaM_realloc_函数的宏luaM_reallocv,该宏针对数组操作,根据新的数组元素个数重新分配内存
+// size当前数组大小 limit数组大小上限
 void *luaM_growaux_ (lua_State *L, void *block, int *size, size_t size_elems,
                      int limit, const char *what) {
   void *newblock;
   int newsize;
-  if (*size >= limit/2) {  /* cannot double it? */
-    if (*size >= limit)  /* cannot grow even a little? */
+  if (*size >= limit/2) {  /* cannot double it? 大小不能翻倍 */
+    if (*size >= limit)  /* cannot grow even a little? size已经达到了最大值 */
       luaG_runerror(L, "too many %s (limit is %d)", what, limit);
-    newsize = limit;  /* still have at least one free place */
+    newsize = limit;  /* still have at least one free place 虽然不能翻倍但是也能扩张一些到limit */
   }
   else {
     newsize = (*size)*2;
     if (newsize < MINSIZEARRAY)
       newsize = MINSIZEARRAY;  /* minimum size */
   }
+  // 重新分配空间
   newblock = luaM_reallocv(L, block, *size, newsize, size_elems);
-  *size = newsize;  /* update only when everything else is OK */
+  *size = newsize;  /* update only when everything else is OK 修改size */
   return newblock;
 }
 
@@ -74,29 +86,38 @@ l_noret luaM_toobig (lua_State *L) {
 /*
 ** generic allocation routine.
 */
-// 扩充重新分配block指向的空间为nsize, block的值有可能改变所以需要调用处重新赋值
-// 如果block为NULL,那么osize没有意义,直接分配nsize大小的空间
+// 通用内存分配函数
+// block需要重新分配的内存 osize old size, nsize new size
+// block 不为NULL, osize block的大小, nsize 新大小 对block重新分配
+// block 不为NULL, osize block的大小, nsize 0  释放block
+// block NULL, osize 任意值, nsize 新大小  分配一块nsize大小的空间并返回
+// block NULL, osize 任意值, nsize 0  等价于free(NULL) 什么也不做
 void *luaM_realloc_ (lua_State *L, void *block, size_t osize, size_t nsize) {
   void *newblock;
   global_State *g = G(L);
-  size_t realosize = (block) ? osize : 0;
+  size_t realosize = (block) ? osize : 0;  // block为NULL 说明osize是0
   lua_assert((realosize == 0) == (block == NULL));
 #if defined(HARDMEMTESTS)
   if (nsize > realosize && g->gcrunning)
     luaC_fullgc(L, 1);  /* force a GC whenever possible */
 #endif
   newblock = (*g->frealloc)(g->ud, block, osize, nsize);
-  if (newblock == NULL && nsize > 0) {
-    lua_assert(nsize > realosize);  /* cannot fail when shrinking a block */
-    if (g->version) {  /* is state fully built? */
+  if (newblock == NULL && nsize > 0) { // 内存分配失败
+    lua_assert(nsize > realosize);  /* cannot fail when shrinking a block 缩小空间时不应该会失败 */
+    if (g->version) {  /* is state fully built? 尝试gc后再次进行分配内存 */
       luaC_fullgc(L, 1);  /* try to free some memory... */
+      // g->frealloc 实际函数 l_alloc
+      // nsize是0   调用free(block)
+      // nsize不是0 调用realloc(block, nsize)
       newblock = (*g->frealloc)(g->ud, block, osize, nsize);  /* try again */
     }
-    if (newblock == NULL)
+    if (newblock == NULL)  // 重新分配还是失败 抛出错误
       luaD_throw(L, LUA_ERRMEM);
   }
+  // nsize是0 newblock是NULL
+  // nsize不是0 newblock一定不是NULL
   lua_assert((nsize == 0) == (newblock == NULL));
-  g->GCdebt = (g->GCdebt + nsize) - realosize;
+  g->GCdebt = (g->GCdebt + nsize) - realosize; // GCdebt 加上新增加的空间
   return newblock;
 }
 
--- lmem.h
+++ lmem.h
@@ -1,6 +1,6 @@
 /*
 ** $Id: lmem.h,v 1.43 2014/12/19 17:26:14 roberto Exp $
-** Interface to Memory Manager
+** Interface to Memory Manager 内存管理接口
 ** See Copyright Notice in lua.h
 */
 
@@ -27,6 +27,8 @@
 ** false due to limited range of data type"; the +1 tricks the compiler,
 ** avoiding this warning but also this optimization.)
 */
+// 这个宏重新分配数组b的内存 从on个元素到n个元素 每个元素的大小是e
+// (n+1) > MAX_SIZET/e 目的是确保n*e不会溢出
 #define luaM_reallocv(L,b,on,n,e) \
   (((sizeof(n) >= sizeof(size_t) && cast(size_t, (n)) + 1 > MAX_SIZET/(e)) \
       ? luaM_toobig(L) : cast_void(0)) , \
@@ -35,24 +37,37 @@
 /*
 ** Arrays of chars do not need any test
 */
+// 分配字符串数组
 #define luaM_reallocvchar(L,b,on,n)  \
     cast(char *, luaM_realloc_(L, (b), (on)*sizeof(char), (n)*sizeof(char)))
 
+// 释放内存 指定块的大小 不需要传nsize了 一定是0
 #define luaM_freemem(L, b, s)	luaM_realloc_(L, (b), (s), 0)
+// 释放内存 不指定块的大小 释放的大小就是块的结构体的大小
 #define luaM_free(L, b)		luaM_realloc_(L, (b), sizeof(*(b)), 0)
+// 释放数组的内存 传入指针和元素的个数
 #define luaM_freearray(L, b, n)   luaM_realloc_(L, (b), (n)*sizeof(*(b)), 0)
 
+// 分配s大小的内存
 #define luaM_malloc(L,s)	luaM_realloc_(L, NULL, 0, (s))
+// 分配t类型大小的内存 并进行类型转换
 #define luaM_new(L,t)		cast(t *, luaM_malloc(L, sizeof(t)))
+// 分配t类型n个元素数组
 #define luaM_newvector(L,n,t) \
 		cast(t *, luaM_reallocv(L, NULL, 0, n, sizeof(t)))
 
+// 直接分配s大小内存 tag没有实际意义只是为了调用处的可读性
 #define luaM_newobject(L,tag,s)	luaM_realloc_(L, NULL, tag, (s))
 
+// 在数组已经放满的情况下增长数组的大小 一般是进行翻倍
+// v要增长的数组 nelems数组中当前元素的个数 size数组当前的大小
+// t数组元素类型 limit数组大小上限 e用于错误信息
 #define luaM_growvector(L,v,nelems,size,t,limit,e) \
           if ((nelems)+1 > (size)) \
             ((v)=cast(t *, luaM_growaux_(L,v,&(size),sizeof(t),limit,e)))
 
+// 重新分配数组大小并进行类型转换
+// v 要重新分配的数组 oldn分配前元素个数 n要分配的元素个数 t元素的类型
 #define luaM_reallocvector(L, v,oldn,n,t) \
    ((v)=cast(t *, luaM_reallocv(L, v, oldn, n, sizeof(t))))
 

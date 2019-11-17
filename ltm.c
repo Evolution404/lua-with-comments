50bfe7063bca113eded8b8fffff096c0ba70a1ce
zhangyuxi <615598813.com>
2019-11-17 14:01:37 +0800

    add ltm

--- ltm.c
+++ ltm.c
@@ -34,8 +34,9 @@ LUAI_DDEF const char *const luaT_typenames_[LUA_TOTALTAGS] = {
 };
 
 
+// 初始化元方法 在Lua虚拟机中注册元方法的名称并保证它们不会被回收
 void luaT_init (lua_State *L) {
-  static const char *const luaT_eventname[] = {  /* ORDER TM */
+  static const char *const luaT_eventname[] = {  /* ORDER TM 所有元方法的名称 */
     "__index", "__newindex",
     "__gc", "__mode", "__len", "__eq",
     "__add", "__sub", "__mul", "__mod", "__pow",
@@ -47,7 +48,7 @@ void luaT_init (lua_State *L) {
   int i;
   for (i=0; i<TM_N; i++) {
     G(L)->tmname[i] = luaS_new(L, luaT_eventname[i]);
-    luaC_fix(L, obj2gco(G(L)->tmname[i]));  /* never collect these names */
+    luaC_fix(L, obj2gco(G(L)->tmname[i]));  /* never collect these names 不要对这些值进行垃圾回收 */
   }
 }
 
@@ -56,29 +57,36 @@ void luaT_init (lua_State *L) {
 ** function to be used with macro "fasttm": optimized for absence of
 ** tag methods
 */
+// 查询元方法的第一种方式
+// 用于fasttm查询元方法 使用了 Table 结构中flags项进行缓存
 const TValue *luaT_gettm (Table *events, TMS event, TString *ename) {
-  const TValue *tm = luaH_getshortstr(events, ename);
+  const TValue *tm = luaH_getshortstr(events, ename);  // 从表中查询表项
   lua_assert(event <= TM_EQ);
-  if (ttisnil(tm)) {  /* no tag method? */
-    events->flags |= cast_byte(1u<<event);  /* cache this fact */
+  if (ttisnil(tm)) {  /* no tag method? 没有查询到元方法进行缓存 */
+    events->flags |= cast_byte(1u<<event);  /* cache this fact 在flags项上设置该元方法位置为1 */
     return NULL;
   }
   else return tm;
 }
 
-
+// 查询元方法的第二种方式 可以查询任意对象中的任意元方法
+// lua中的每个值都可以有一个元表 只是table和userdata可以有各自独立的元表 而其他类型的值则共享其类型所属的单一元表
+// lua代码中只能设置table的元表 至于其他类型值的元表只能通过C代码设置
 const TValue *luaT_gettmbyobj (lua_State *L, const TValue *o, TMS event) {
   Table *mt;
-  switch (ttnov(o)) {
-    case LUA_TTABLE:
+  switch (ttnov(o)) {   // 判断o的主类型
+    case LUA_TTABLE:    // 获取Table类型的元表
       mt = hvalue(o)->metatable;
       break;
-    case LUA_TUSERDATA:
+    case LUA_TUSERDATA: // 获取Userdata类型的元表
       mt = uvalue(o)->metatable;
       break;
-    default:
+    default:            // 其他基础类型的元表
       mt = G(L)->mt[ttnov(o)];
   }
+  // 从元表中取出要查询的元方法 就是上一个函数的第一句 少了缓存的过程
+  // 上一个函数是在元表中查询元方法
+  // 这个函数是要先找出元表 再在元表中查询元方法
   return (mt ? luaH_getshortstr(mt, G(L)->tmname[event]) : luaO_nilobject);
 }
 
@@ -87,6 +95,15 @@ const TValue *luaT_gettmbyobj (lua_State *L, const TValue *o, TMS event) {
 ** Return the name of the type of an object. For tables and userdata
 ** with metatable, use their '__name' metafield, if present.
 */
+// 返回一个TValue的类型名 如果table和userdata的元表中有__name键那么就使用这个键的值
+// 这个函数并不是lua内置函数type调用的 只有在输出调试信息(ldebug.c文件)时会调用
+/* lua 代码
+   a = {}
+   m = {__name = 'hello'}
+   setmetatable(a, m)
+   a < a
+   会打印报错信息 attempt to compare two hello values
+ */
 const char *luaT_objtypename (lua_State *L, const TValue *o) {
   Table *mt;
   if ((ttistable(o) && (mt = hvalue(o)->metatable) != NULL) ||
--- ltm.h
+++ ltm.h
@@ -15,9 +15,10 @@
 * WARNING: if you change the order of this enumeration,
 * grep "ORDER TM" and "ORDER OP"
 */
+// 所有的标签方法
 typedef enum {
-  TM_INDEX,
-  TM_NEWINDEX,
+  TM_INDEX,     // 用于查询不存在的key
+  TM_NEWINDEX,  // 用于设置不存在的key 在给不存在的key赋值时会调用
   TM_GC,
   TM_MODE,
   TM_LEN,
@@ -43,16 +44,25 @@ typedef enum {
   TM_N		/* number of elements in the enum */
 } TMS;
 
+//查询元表的方式有两种：
+// 方式一、查询某个元表（Table结构体）的元方法（见fasttm, gfasttm）
+//         使用了缓存可以快速查询 但是只能查询TM_EQ以及它之前的元方法
+// 方式二、查询某个对象的元方法（见luaT_gettmbyobj）
+//         可以查询所有的元方法
 
-
+// 查询某个元表（Table结构体）的元方法
+// et是要查询的元表 e是要查询的事件(元方法的枚举值)
+// 如果et是NULL或者flags中缓存结果是NULL那么直接返回NULL
+// 否则使用luaT_gettm查询元方法
 #define gfasttm(g,et,e) ((et) == NULL ? NULL : \
   ((et)->flags & (1u<<(e))) ? NULL : luaT_gettm(et, e, (g)->tmname[e]))
 
+// 从et中查询元方法e 查询不到返回NULL
 #define fasttm(l,et,e)	gfasttm(G(l), et, e)
 
 #define ttypename(x)	luaT_typenames_[(x) + 1]
 
-LUAI_DDEC const char *const luaT_typenames_[LUA_TOTALTAGS];
+LUAI_DDEC const char *const luaT_typenames_[LUA_TOTALTAGS]; // 各种数据类型的名称
 
 
 LUAI_FUNC const char *luaT_objtypename (lua_State *L, const TValue *o);

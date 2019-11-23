76ce6065133f11fc7feef551e56f1414fe866c7a
zhangyuxi <615598813.com>
2019-11-23 11:14:02 +0800

    add lstring

--- lstring.c
+++ lstring.c
@@ -30,6 +30,7 @@
 ** Lua will use at most ~(2^LUAI_HASHLIMIT) bytes from a string to
 ** compute its hash
 */
+// 最多会使用32(2^5)字节去计算一个字符串的哈希值
 #if !defined(LUAI_HASHLIMIT)
 #define LUAI_HASHLIMIT		5
 #endif
@@ -40,6 +41,7 @@
 */
 
 // 长字符串相等判断
+// 先比较两者指针是否相同 如果不同再比较实际的值是否相同
 int luaS_eqlngstr (TString *a, TString *b) {
   size_t len = a->u.lnglen;
   lua_assert(a->tt == LUA_TLNGSTR && b->tt == LUA_TLNGSTR);
@@ -61,11 +63,12 @@ unsigned int luaS_hash (const char *str, size_t l, unsigned int seed) {
 }
 
 
+// 计算并设置长字符串的哈希值 返回计算哈希值
 unsigned int luaS_hashlongstr (TString *ts) {
   lua_assert(ts->tt == LUA_TLNGSTR);
   if (ts->extra == 0) {  /* no hash? */
-    ts->hash = luaS_hash(getstr(ts), ts->u.lnglen, ts->hash);
-    ts->extra = 1;  /* now it has its hash */
+    ts->hash = luaS_hash(getstr(ts), ts->u.lnglen, ts->hash);  // ts->hash初始被赋值为G(L)->seed
+    ts->extra = 1;  /* now it has its hash 标记已经被计算了哈希值 */
   }
   return ts->hash;
 }
@@ -74,7 +77,8 @@ unsigned int luaS_hashlongstr (TString *ts) {
 /*
 ** resizes the string table
 */
-// string table 重新分配空间
+// 为stringtable重新分配空间 可以扩大或缩小
+// 空间扩大缩小后各个值存放的哈希桶也会一起重新计算
 void luaS_resize (lua_State *L, int newsize) {
   int i;
   stringtable *tb = &G(L)->strt;
@@ -85,20 +89,21 @@ void luaS_resize (lua_State *L, int newsize) {
     for (i = tb->size; i < newsize; i++)
       tb->hash[i] = NULL;
   }
-  // 重新排列TString位置
-  for (i = 0; i < tb->size; i++) {  /* rehash */
+  // 针对newsize对各个桶内的值重新计算应该属于哪个桶
+  for (i = 0; i < tb->size; i++) {  /* rehash 遍历每一个桶 */
     TString *p = tb->hash[i];
     tb->hash[i] = NULL;
-    while (p) {  /* for each node in the list */
-      TString *hnext = p->u.hnext;  /* save next */
-      unsigned int h = lmod(p->hash, newsize);  /* new position */
-      p->u.hnext = tb->hash[h];  /* chain it */
-      tb->hash[h] = p;
-      p = hnext;
+    while (p) {  /* for each node in the list 遍历当前桶的每一个节点 */
+      TString *hnext = p->u.hnext;  /* save next 保存当前节点的下一个位置 */
+      unsigned int h = lmod(p->hash, newsize);  /* new position 计算当前节点的应该位于哪个新的桶 */
+      p->u.hnext = tb->hash[h];  /* chain it 插入新的桶之前保存新桶的头节点 */
+      tb->hash[h] = p;  // 当前节点插入新的桶
+      p = hnext;  // 访问当前桶的下一个节点
     }
   }
   if (newsize < tb->size) {  /* shrink table if needed */
     /* vanishing slice should be empty */
+    // 上面已经针对newsize个桶进行了重新分配,缩小的部分不会有值
     lua_assert(tb->hash[newsize] == NULL && tb->hash[tb->size - 1] == NULL);
     luaM_reallocvector(L, tb->hash, tb->size, newsize, TString *);
   }
@@ -110,6 +115,7 @@ void luaS_resize (lua_State *L, int newsize) {
 ** Clear API string cache. (Entries cannot be empty, so fill them with
 ** a non-collectable string.)
 */
+// 清理缓存g->strcache
 void luaS_clearcache (global_State *g) {
   int i, j;
   for (i = 0; i < STRCACHE_N; i++)
@@ -123,13 +129,16 @@ void luaS_clearcache (global_State *g) {
 /*
 ** Initialize the string table and the string cache
 */
+// 初始化g->memerrmsg g->stringtable g->strcache
 void luaS_init (lua_State *L) {
   global_State *g = G(L);
   int i, j;
+  // 初始化stringtable
   luaS_resize(L, MINSTRTABSIZE);  /* initial size of string table */
   /* pre-create memory-error message */
   g->memerrmsg = luaS_newliteral(L, MEMERRMSG);
   luaC_fix(L, obj2gco(g->memerrmsg));  /* it should never be collected */
+  // 初始化strcache
   for (i = 0; i < STRCACHE_N; i++)  /* fill cache with valid strings */
     for (j = 0; j < STRCACHE_M; j++)
       g->strcache[i][j] = g->memerrmsg;
@@ -140,20 +149,23 @@ void luaS_init (lua_State *L) {
 /*
 ** creates a new string object
 */
+// 创建一个空的TString对象,已经为将要存放的字符串申请了空间
+// 由于不一定是长字符串还是短字符串所以没有指定长度
 static TString *createstrobj (lua_State *L, size_t l, int tag, unsigned int h) {
   TString *ts;
   GCObject *o;
-  size_t totalsize;  /* total size of TString object */
+  size_t totalsize;  /* total size of TString object 一个TString对象的全部大小包括后面存储的字符串 */
   totalsize = sizelstring(l);
   o = luaC_newobj(L, tag, totalsize);
   ts = gco2ts(o);
   ts->hash = h;
-  ts->extra = 0;
-  getstr(ts)[l] = '\0';  /* ending 0 */
+  ts->extra = 0;  // 长字符串标记还没有计算哈希值 短字符串标记不是关键字
+  getstr(ts)[l] = '\0';  /* ending 0 标记字符串结尾 */
   return ts;
 }
 
 
+// 创建长字符串 在对象中明确指定字符串的长度
 TString *luaS_createlngstrobj (lua_State *L, size_t l) {
   TString *ts = createstrobj(L, l, LUA_TLNGSTR, G(L)->seed);
   ts->u.lnglen = l;
@@ -161,11 +173,15 @@ TString *luaS_createlngstrobj (lua_State *L, size_t l) {
 }
 
 
+// 从stringtable中删除ts
 void luaS_remove (lua_State *L, TString *ts) {
   stringtable *tb = &G(L)->strt;
-  TString **p = &tb->hash[lmod(ts->hash, tb->size)];
+  TString **p = &tb->hash[lmod(ts->hash, tb->size)];  // 找到删除的对象所在的桶
   while (*p != ts)  /* find previous element */
     p = &(*p)->u.hnext;
+  // 从链表中删除元素可以使用二级指针
+  // 现在p就是ts上一个节点的netx域的指针
+  // *p==ts 修改*p=(*p)->next 自然它的上一个节点也会修改
   *p = (*p)->u.hnext;  /* remove element from its list */
   tb->nuse--;
 }
@@ -174,25 +190,29 @@ void luaS_remove (lua_State *L, TString *ts) {
 /*
 ** checks whether short string exists and reuses it or creates a new one
 */
+// 创建短字符串
+// 查询短字符串是否存在 存在的话复用 不存在就新建
 static TString *internshrstr (lua_State *L, const char *str, size_t l) {
   TString *ts;
   global_State *g = G(L);
   unsigned int h = luaS_hash(str, l, g->seed);
-  TString **list = &g->strt.hash[lmod(h, g->strt.size)];
+  TString **list = &g->strt.hash[lmod(h, g->strt.size)];  // 找到所在的桶
   lua_assert(str != NULL);  /* otherwise 'memcmp'/'memcpy' are undefined */
-  for (ts = *list; ts != NULL; ts = ts->u.hnext) {
+  for (ts = *list; ts != NULL; ts = ts->u.hnext) {  // 遍历桶的节点
     if (l == ts->shrlen &&
         (memcmp(str, getstr(ts), l * sizeof(char)) == 0)) {
-      /* found! */
+      /* found! 长度相同 内容完全一样 */
       if (isdead(g, ts))  /* dead (but not collected yet)? */
         changewhite(ts);  /* resurrect it */
-      return ts;
+      return ts;  // 直接复用
     }
   }
+  // 检查stringtable的空间是否足够
   if (g->strt.nuse >= g->strt.size && g->strt.size <= MAX_INT/2) {
-    luaS_resize(L, g->strt.size * 2);
-    list = &g->strt.hash[lmod(h, g->strt.size)];  /* recompute with new size */
+    luaS_resize(L, g->strt.size * 2);  // 翻倍扩容
+    list = &g->strt.hash[lmod(h, g->strt.size)];  /* recompute with new size 扩容后需要重新计算桶 */
   }
+  // 创建新的短字符串
   ts = createstrobj(L, l, LUA_TSHRSTR, h);
   memcpy(getstr(ts), str, l * sizeof(char));
   ts->shrlen = cast_byte(l);
@@ -206,10 +226,12 @@ static TString *internshrstr (lua_State *L, const char *str, size_t l) {
 /*symotion-prefix)
 ** new string (with explicit length)
 */
+// 创建字符串(显式指定长度)
+// 这个函数不会查询缓存
 TString *luaS_newlstr (lua_State *L, const char *str, size_t l) {
-  if (l <= LUAI_MAXSHORTLEN)  /* short string? */
+  if (l <= LUAI_MAXSHORTLEN)  /* short string? 创建短字符串 */
     return internshrstr(L, str, l);
-  else {
+  else {  // 创建长字符串
     TString *ts;
     if (l >= (MAX_SIZE - sizeof(TString))/sizeof(char))
       luaM_toobig(L);
@@ -227,6 +249,7 @@ TString *luaS_newlstr (lua_State *L, const char *str, size_t l) {
 ** check hits.
 */
 // 使用这个函数创建字符串直接去cache中找,有相同的就不再创建
+// cache在同一个桶内是先进先出的 进来一个把最后一个挤出去
 TString *luaS_new (lua_State *L, const char *str) {
   unsigned int i = point2uint(str) % STRCACHE_N;  /* hash */
   int j;
@@ -236,14 +259,16 @@ TString *luaS_new (lua_State *L, const char *str) {
       return p[j];  /* that is it */
   }
   /* normal route */
+  // 在cache中没找到 在新创建之前先从cache中删掉一个用来存新创建的
   for (j = STRCACHE_M - 1; j > 0; j--)
-    p[j] = p[j - 1];  /* move out last element */
+    p[j] = p[j - 1];  /* move out last element 全部向后移一个 最终删掉最后一个 */
   /* new element is first in the list */
   p[0] = luaS_newlstr(L, str, strlen(str));
   return p[0];
 }
 
 
+// userdata就是一块原始内存 没有存放任何东西
 Udata *luaS_newudata (lua_State *L, size_t s) {
   Udata *u;
   GCObject *o;
--- lstring.h
+++ lstring.h
@@ -12,11 +12,17 @@
 #include "lstate.h"
 
 
+// TString对象需要的空间是结构体需要空间+字符串长度+1(存放\0)
+// l是存放字符串的长度(不包括\0)
 #define sizelstring(l)  (sizeof(union UTString) + ((l) + 1) * sizeof(char))
 
+// Udata对象需要的空间包括 结构体占用空间+存放数据的大小
+// l是Udata存放数据的大小
 #define sizeludata(l)	(sizeof(union UUdata) + (l))
+// u是Udata对象 计算该对象总共占用的空间 包括了实际存放的数据占用的空间
 #define sizeudata(u)	sizeludata((u)->len)
 
+// 用于创建一个明确文本的字符串 例如 luaS_newliteral(L, "hello")
 #define luaS_newliteral(L, s)	(luaS_newlstr(L, "" s, \
                                  (sizeof(s)/sizeof(char))-1))
 
@@ -24,12 +30,16 @@
 /*
 ** test whether a string is a reserved word
 */
+// 检查一个TString对象是否是关键字
+// 是短字符串且extra为0 说明是关键字
 #define isreserved(s)	((s)->tt == LUA_TSHRSTR && (s)->extra > 0)
 
 
 /*
 ** equality for short strings, which are always internalized
 */
+// 比较短字符串是否相等
+// a,b是TString对象 短字符串都被内化到虚拟机内 字符串相等一定地址相等 只需要比较地址
 #define eqshrstr(a,b)	check_exp((a)->tt == LUA_TSHRSTR, (a) == (b))
 
 
